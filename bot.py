b2ceff095706c32ca33011ee28253d0fb521e69f
Poco Pump <danielharoon1@gmail.com>
2019-06-24 19:15:03 -0400

    added .clear, error handling, and welcome

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 import os
 
-client = commands.Bot(command_prefix = '.')
+client = commands.Bot(command_prefix = '.', case_insensitive = True)
 
 @client.event
 async def on_ready():
@@ -10,6 +10,8 @@ async def on_ready():
 
 @client.event
 async def on_member_join(member):
+    channel = client.get_channel(592853778538037248)
+    await channel.send('hello')
     print(f'{member} has joined a server')
 
 @client.event
@@ -20,8 +22,22 @@ async def on_member_remove(member):
 async def kick(ctx,member:discord.Member,*,reason=None):
     await member.kick(reason = reason)
     print(f'kicked {member} from {ctx.Message.guild}')
+
 @client.command()
-async def kick(ctx,member:discord.Member,*,reason=None):
+async def ban(ctx,member:discord.Member,*,reason=None):
     await member.ban(reason = reason)
     print(f'banned {member} from {ctx.Message.guild}')
+
+@client.command()
+async def clear(ctx,amount:int):
+    await ctx.channel.purge(limit = amount)
+
+@client.event
+async def on_command_error(ctx,error):
+    pass
+
+@clear.error
+async def clear_error(ctx, error):
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send('specify an amound of messages to delete')
 client.run(os.environ.get('MODSAREGAY'))
