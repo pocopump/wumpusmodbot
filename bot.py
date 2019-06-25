14590c30746cd0e408f5e574ac336440a853e599
Poco Pump <danielharoon1@gmail.com>
2019-06-24 20:03:48 -0400

    tweaked welcome added clear every minute, kinda buggy

--- bot.py
+++ bot.py
@@ -1,17 +1,20 @@
 import discord
-from discord.ext import commands
+from discord.ext import commands,tasks
 import os
+import random
 
+welcomes = ['epic gamer moment','welcome','ugh, another person to babysit']
 client = commands.Bot(command_prefix = '.', case_insensitive = True)
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+    await client.change_presence(status = discord.Status.online, activity=discord.Game('i literally live in powershell'))
 
 @client.event
 async def on_member_join(member):
     channel = client.get_channel(592853778538037248)
-    await channel.send('hello')
+    await channel.send(f'{member} has joined the server ')
     print(f'{member} has joined a server')
 
 @client.event
@@ -40,4 +43,12 @@ async def on_command_error(ctx,error):
 async def clear_error(ctx, error):
     if isinstance(error, commands.MissingRequiredArgument):
         await ctx.send('specify an amound of messages to delete')
+
+@tasks.loop(minutes=1)
+async def client_clear():
+    channel = client.get_channel(592144260888002565)
+    await channel.purge(limit = 5000)
+    print('cleared all messages in #general')
+
+
 client.run(os.environ.get('MODSAREGAY'))
