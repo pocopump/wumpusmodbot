2b14a4c2ea695598e877b5787249f340fa83dc10
Poco Pump <danielharoon1@gmail.com>
2019-06-26 21:34:49 -0400

    edited a bunch of stuff, changed it so that you have to be moderator to use certain commands

--- bot.py
+++ bot.py
@@ -2,8 +2,8 @@ import discord
 from discord.ext import commands,tasks
 import os
 import random
-
-welcomes = ['epic gamer moment','welcome','ugh, another person to babysit']
+global str
+welcomes = ['epic gamer moment','welcome','enjoy your stay']
 client = commands.Bot(command_prefix = '.', case_insensitive = True)
 
 @client.event
@@ -22,33 +22,33 @@ async def on_member_remove(member):
     print(f'{member} has left a server')
 
 @client.command()
+@commands.has_role("moderator")
 async def kick(ctx,member:discord.Member,*,reason=None):
     await member.kick(reason = reason)
     print(f'kicked {member} from {ctx.Message.guild}')
 
 @client.command()
+@commands.has_role("moderator")
 async def ban(ctx,member:discord.Member,*,reason=None):
     await member.ban(reason = reason)
-    print(f'banned {member} from {ctx.Message.guild}')
+    print(f'banned {member} from {ctx.message.guild}')
 
 @client.command()
+@commands.has_role("moderator")
 async def clear(ctx,amount:int):
     await ctx.channel.purge(limit = amount)
-
+    print(f'cleared {amount} message(s)')
+'''
 @client.event
 async def on_command_error(ctx,error):
     pass
-
+'''
 @clear.error
 async def clear_error(ctx, error):
     if isinstance(error, commands.MissingRequiredArgument):
         await ctx.send('specify an amound of messages to delete')
 
-@tasks.loop(minutes=1)
-async def client_clear():
-    channel = client.get_channel(592144260888002565)
-    await channel.purge(limit = 5000)
-    print('cleared all messages in #general')
+
 
 
 client.run(os.environ.get('MODSAREGAY'))
