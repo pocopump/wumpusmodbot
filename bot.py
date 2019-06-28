e119691b9006dbcd0746f0dc00a80a46f6879535
Poco Pump <danielharoon1@gmail.com>
2019-06-27 23:57:28 -0400

    unfinished but i have to submit :(

--- bot.py
+++ bot.py
@@ -2,9 +2,11 @@ import discord
 from discord.ext import commands,tasks
 import os
 import random
-global str
-welcomes = ['epic gamer moment','welcome','enjoy your stay']
-client = commands.Bot(command_prefix = '.', case_insensitive = True)
+import asyncio
+from discord.ext.commands import Bot
+
+bad_words = ['nigga','bitch','whore','twat','fuck','shit','nigger']
+client = Bot(command_prefix = '?', case_insensitive = True)
 
 @client.event
 async def on_ready():
@@ -34,7 +36,6 @@ async def ban(ctx,member:discord.Member,*,reason=None):
     print(f'banned {member} from {ctx.message.guild}')
 
 @client.command()
-@commands.has_role("moderator")
 async def clear(ctx,amount:int):
     await ctx.channel.purge(limit = amount)
     print(f'cleared {amount} message(s)')
@@ -49,6 +50,17 @@ async def clear_error(ctx, error):
         await ctx.send('specify an amound of messages to delete')
 
 
+@client.event
+async def on_message(message):
+    for bad_word in bad_words:
+        if bad_word in message.content.strip().lower():
+            await message.channel.send("{}, your message has been censored.".format(message.author.mention))
+            await message.delete()
 
+'''
+@client.command()
+@commands.has_role("moderator")
+async def vote():
+'''
 
 client.run(os.environ.get('MODSAREGAY'))
